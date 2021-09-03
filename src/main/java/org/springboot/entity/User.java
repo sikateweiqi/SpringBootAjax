998e2751f81396c1b1284509ec5407d6e0506d62
曹晶 <sikateweiqi@163.com>
2021-09-04 06:19:26 +0800

    add dependency

--- src/main/java/org/springboot/entity/User.java
+++ src/main/java/org/springboot/entity/User.java
@@ -1,53 +1,19 @@
 package org.springboot.entity;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
-    private Integer id;
+    Integer id;
 
-    private String name;
+    String name;
 
-    private String sex;
+    String sex;
 
-    private Integer age;
+    Integer age;
 
-    private String role;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+    String role;
 }
