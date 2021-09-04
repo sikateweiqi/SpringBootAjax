ee1b54901ac61fef0941041683fc72aad0ec5046
曹晶 <sikateweiqi@163.com>
2021-09-04 20:13:23 +0800

    增加缓存

--- src/main/java/com/spdb/scc/service/impl/LoadNodeDataServiceImpl.java
+++ src/main/java/com/spdb/scc/service/impl/LoadNodeDataServiceImpl.java
@@ -1,17 +1,24 @@
 package com.spdb.scc.service.impl;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.spdb.scc.dao.IpLogDao;
 import com.spdb.scc.dao.LoadNodeDao;
 import com.spdb.scc.entity.ClusterNodeMap;
 import com.spdb.scc.entity.Nodes;
 import com.spdb.scc.service.IpsService;
 import com.spdb.scc.service.LoadNodeDataService;
+import com.spdb.scc.utils.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -26,11 +33,28 @@ public class LoadNodeDataServiceImpl implements LoadNodeDataService {
     @Autowired
     private IpLogDao ipLogDao;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
     public List<ClusterNodeMap> loadAllNode() {
         List<ClusterNodeMap> list = new ArrayList<>();
+
+        Object obj = redisTemplate.opsForValue().get(Constants.REDIS_CLUSTER_NODE_KEY);
+
+//        list = (List<ClusterNodeMap>) redisTemplate.opsForValue().get(Constants.REDIS_CLUSTER_NODE_KEY);
+
+        if (obj != null){
+            //从redis获取;
+            String redis_value = obj.toString();
+            list = new Gson().fromJson(redis_value,new TypeToken<List<ClusterNodeMap>>(){}.getType());
+            return list;
+        }
+
         try {
+            //从数据库获取并写入缓存
             list = loadNodeDao.loadAllNode();
+            redisTemplate.opsForValue().set(Constants.REDIS_CLUSTER_NODE_KEY,new Gson().toJson(list), 5,TimeUnit.MINUTES);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -56,11 +80,23 @@ public class LoadNodeDataServiceImpl implements LoadNodeDataService {
     }
 
     private boolean isIpInWhiteList(String ip) {
-        List<String> white_list = ipsService.whiteList();
+        List<String> white_list = new ArrayList<>();
 
         addIpLog(ip);
 
-        if (white_list.isEmpty()) {
+        Object obj =  redisTemplate.opsForValue().get(Constants.REDIS_PWD_WHITE_LIST_KEY);
+        if (obj == null){
+            //从数据库获取;
+            white_list = ipsService.whiteList();
+            redisTemplate.opsForValue().set(Constants.REDIS_PWD_WHITE_LIST_KEY,new Gson().toJson(white_list), 5, TimeUnit.MINUTES);
+
+        }else {
+            //从redis获取
+            String redis_value = obj.toString();
+            white_list = new Gson().fromJson(redis_value,new TypeToken<List<String>>(){}.getType());
+        }
+
+        if (white_list == null || white_list.isEmpty()) {
             return false;
         }
 
@@ -73,6 +109,16 @@ public class LoadNodeDataServiceImpl implements LoadNodeDataService {
 
     private boolean addIpLog(String ip){
         return ipLogDao.insert(ip);
+    }
+
+    @Autowired(required = false)
+    public void setRedisTemplate(RedisTemplate redisTemplate) {
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setValueSerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+        this.redisTemplate = redisTemplate;
     }
 
 
---
+++ src/main/java/com/spdb/scc/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.spdb.scc.utils;
+
+public class Constants {
+    public static final String REDIS_CLUSTER_NODE_KEY = "cluster_node_map";
+
+    public static final String REDIS_PWD_WHITE_LIST_KEY = "pwd_white_list";
+}
